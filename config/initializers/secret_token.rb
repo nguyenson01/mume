8ea2d9633ab54c41d6d4bec82556b826d5b260aa
nguyenson01 <son.nguyen@mail.rakuten.com>
2013-03-05 21:37:39 +0900

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Mume::Application.config.secret_token = 'e6529301a60a4c20b17bf05096723000c8a9a857c640da4f230e7602ab7a7f9236f7fc08c235df7062dda195141b4f8af41efe4e833402556e7267de229bf0d4'
